b2223ef5a60abfd02f09445f241974bb3a21467b
Jaasim <479jaasim@gmail.com>
2025-08-12 12:55:10 +0530

    Project file structure created and dependencies installed

---
+++ frontend/src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import REACTDOM from 'react-dom/client'
+import {BrowserRouter} from 'react-router-dom'
+import { Provider } from 'react-redux'
+// import {store} from './store/store.js'
+import './index.css'
+import App from './App.jsx'
+
+REACTDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </Provider>
+//   </React.StrictMode>,
+// )
